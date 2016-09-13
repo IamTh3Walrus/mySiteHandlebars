fa714fdf46db9f32199b154cec0fa10487ab23e0
Stephen Doe <redcard1016@yahoo.com>
2016-09-12 21:07:48 -0400

    Things are happening

--- server.js
+++ server.js
@@ -2,22 +2,25 @@
 var express = require('express');
 
 var app = express();
-var api_key = 'key-9d577efadf583cb225247268afc47cb4';
-var domain = 'mydomain.mailgun.org';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+//Connect to MongoBD to recieve email messages.
+var mongo = require('Mongodb');
 
 //Blocks header from containing information about the server.
 app.disable('x-powered-by');
 
 //Defines handlebars as our main layout.
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 //Body-Parser allows us to use the post method in the contact form.
 app.use(require('body-parser').urlencoded({
-	extended: true
+    extended: true
 }));
+
+var credentials = require('./credentials.js');
+app.use(require('cookie-parser')(credentials.cookieSecret));
 
 //Defines that the app is going to be run on port 3000.
 app.set('port', process.env.PORT || 3000);
@@ -26,31 +29,36 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 //Define routes. Req = request to http. Res = response that is sent back.
-app.get('/', function(req, res){
-	res.render('index');
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
-app.get('/home', function(req, res){
-	res.render('home');
+app.get('/home', function(req, res) {
+    res.render('home');
 });
 
-app.get('/portfolio', function(req, res){
-	res.render('portfolio');
+app.get('/portfolio', function(req, res) {
+    res.render('portfolio');
 });
 
-app.get('/contact', function(req, res){
-	res.render('contact', {csrf: 'CSRF token here'});
+app.get('/contact', function(req, res) {
+    res.render('contact', {
+    csrf: 'CSRF token here'});
 });
 
-app.get('/thankyou', function(req, res){
-	res.render('thankyou');
+app.get('/thankyou', function(req, res) {
+    res.render('thankyou');
 });
 
-app.post('/contact.php', function(req, res){
+app.post('/process', function(req, res){
+  console.log('Form : ' + req.query.form);
+  console.log('CSRF token : ' + req.body._csrf);
+  console.log('Email : ' + req.body.email);
+  console.log('Message : ' + req.body.ques);
   res.redirect(303, '/thankyou');
 });
 
 //Tells the app to listen to the port.
-app.listen(app.get('port'), function(){
-	console.log('Express started');
+app.listen(app.get('port'), function() {
+    console.log('Express started');
 });
